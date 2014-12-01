ee1b901577ca2194b1a705cbb6ba5e484744896b
Matteo Merola <mattmezza@gmail.com>
2014-12-01 17:07:58 +0100

    Added an utilities class

---
+++ src/java/it/unimol/my/utils/Utils.java
@@ -0,0 +1,10 @@
+package it.unimol.my.utils;
+
+public class Utils {
+
+	public static final String AUTH_URL_STRING = "https://unimol.esse3.cineca.it/auth/Logon.do";
+	public static final String LOGOUT_URL_STRING = "https://unimol.esse3.cineca.it/Logout.do";
+	
+	public static final String UNISA_AUTH_URL_STRING = "https://esse3web.unisa.it/unisa/auth/Logon.do";
+	public static final String UNISA_LOGOUT_URL_STRING = "https://esse3web.unisa.it/unisa/Logout.do";
+}
