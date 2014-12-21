95c02611f7e9e54bc68e10634b18302dbc0bcdd4
cbranca <c.branca3@gmail.com>
2014-12-21 16:53:28 +0100

    Added extraction of new user informations

--- src/java/it/unimol/my/login/UnimolLoginParser.java
+++ src/java/it/unimol/my/login/UnimolLoginParser.java
@@ -19,140 +19,155 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  * @author Ivan Di Rienzo
  */
 public class UnimolLoginParser implements LoginParser {
+    
+    @Override
+    public UserInformation getLoginInformation(String username, String password) throws UnirestException {
+        
+        HTMLRequester requester = new HTMLRequester();
+        try {
+            
+            String resPage = requester.get(new URL(ConfigurationManager.getInstance().getLogonUrl()),
+                    username, password);
+            
+            Document doc = Jsoup.parse(resPage);
+            
+            if (!this.isLogged(doc)) {
+                
+                return null; // login non riuscito
 
-	@Override
-	public UserInformation getLoginInformation(String username, String password) throws UnirestException {
+            } else {
+                
+                UserInformation uInfo = this.parsingUserInfo(doc);
 
-		HTMLRequester requester = new HTMLRequester();
-		try {
+                // Prende la matricola da un'altra pagina (non login)
+                uInfo.setStudentID(this.getStudentID(username, password));
+                
+                return uInfo;
+            }
+            
+        } catch (MalformedURLException ex) {
+            return null;
+        }
+        
+    }
 
-			String resPage = requester.get(new URL(ConfigurationManager.getInstance().getLogonUrl()),
-					username, password);
+    /**
+     * Metodo privato che fa il parsing dell'html e restituisce un
+     * UserInformation
+     *
+     * @param doc il Document da parsare
+     * @return Le informazioni dell'utente
+     */
+    private UserInformation parsingUserInfo(Document doc) {
+        
+        UserInformation uInfo = new UserInformation();
 
-			Document doc = Jsoup.parse(resPage);
+        // Getting Name And Surname
+        Element name = doc.select("description").first();
+        String[] nameAndSurname = name.html().split("\\u0026nbsp;");
+        uInfo.setName(nameAndSurname[0]);
+        uInfo.setSurname(nameAndSurname[1]);
 
-			if (!this.isLogged(doc)) {
+        // Getting Class
+        Element studentClass = doc.select(
+                "#gu-textStatusStudenteCorsoFac-text-link2").first();
+        Element studentCurrClass = doc.select("p.box-cfu-p description").get(1);
+        uInfo.setStudentClass(studentClass.text() + " "
+                + studentCurrClass.text() + " ANNO");
 
-				return null; // login non riuscito
+        // Getting additional information
+        Elements addInform = doc.select("table[summary$=studente] td");
+        /* scorre gli elementi td memorizzando il precedente, se il precedente
+         * e' l'indicazione di una informazione utile allora vuol dire che
+         * quello attuale e' l'informazione da recuperare
+         */
+        String precedente = "";
+        for (Element tableCell : addInform) {
+            if (precedente.equals("Tasse")) { // situazione tasse es: regolare
+                uInfo.setTaxes(tableCell.text());
+            } else if (precedente.equals("Piano carriera")) {
+                // se il piano carriera e' modificabile
+                uInfo.setCareerPlan(tableCell.text());
+            } else if (precedente.equals("Appelli disponibili")) {
+                // quanti appelli disponibili ci sono
+                uInfo.setAvailableExams(tableCell.text());
+            } else if (precedente.equals("Iscrizioni appelli")) {
+                // a quanti appelli lo studente e' iscritto
+                uInfo.setEnrolledExams(tableCell.text());
+            }
+            precedente = tableCell.text();
+        }
 
-			} else {
+        // getting stats table info
+        Elements userStatusTable = doc.select("div[id=gu-homepagestudente-cp2]");
 
-				UserInformation uInfo = this.parsingUserInfo(doc);
+        // getting course
+        String course = userStatusTable.select("p[id=gu-textStatusStudenteCorsoFac").first().child(0).text();
+        uInfo.setCourse(course);
 
-				// Prende la matricola da un'altra pagina (non login)
-				uInfo.setStudentID(this.getStudentID(username, password));
+        // getting department
+        String department = userStatusTable.select("p[id=gu-textStatusStudenteCorsoFac").first().child(1).text();
+        uInfo.setDepartment(department);
 
-				return uInfo;
-			}
+        // getting course path
+        String coursePath = userStatusTable.select("p[id=gu-textStatusStudenteCorsoFac").first().child(2).text();
+        uInfo.setCoursePath(coursePath);
 
-		} catch (MalformedURLException ex) {
-			return null;
-		}
+        // getting course length
+        String courseLength = userStatusTable.select("p[class=box-cfu-p]").first().child(0).text();
+        uInfo.setCourseLength(courseLength);
 
-	}
+        // getting registration date
+        String registrationDate = userStatusTable.select("p[id=gu-textStatusStudenteImma").first().child(0).text();
+        uInfo.setRegistrationDate(registrationDate);
+        
+        return uInfo;
+    }
 
-	/**
-	 * Metodo privato che fa il parsing dell'html e restituisce un
-	 * UserInformation
-	 *
-	 * @param doc
-	 *            il Document da parsare
-	 * @return Le informazioni dell'utente
-	 */
-	private UserInformation parsingUserInfo(Document doc) {
+    /**
+     * Metodo privato che controlla se il login ha avuto successo
+     *
+     * @param doc il Document da parsare
+     * @return TRUE se il login ha avuto successo FALSE altrimenti
+     */
+    private boolean isLogged(Document doc) {
+        Element firstMeta = doc.select("meta").first();
 
-		UserInformation uInfo = new UserInformation();
+        /* quando il login non ha successo si ottiene una pagina con tag meta
+         * per il redirect
+         */
+        if (firstMeta.attr("http-equiv").equals("refresh")) {
+            // login non riuscito
+            return false;
+        }
+        
+        return true;
+    }
 
-		// Getting Name And Surname
-		Element name = doc.select("description").first();
-		String[] nameAndSurname = name.html().split("\\u0026nbsp;");
-		uInfo.setName(nameAndSurname[0]);
-		uInfo.setSurname(nameAndSurname[1]);
+    /**
+     * Recupera la matricola da un'altra pagina! dato che non e' presente nella
+     * home di esse3
+     *
+     * @return lo studentID
+     */
+    private String getStudentID(String username, String password)
+            throws MalformedURLException, UnirestException {
+        
+        HTMLRequester req = new HTMLRequester();
+        String resPage = req.get(new URL(ConfigurationManager.getInstance().getRecordBookUrl()),
+                username, password);
+        
+        Document doc = Jsoup.parse(resPage);
+        Element name = doc.select("div.titolopagina").first();
 
-		// Getting Class
-		Element studentClass = doc.select(
-				"#gu-textStatusStudenteCorsoFac-text-link2").first();
-		Element studentCurrClass = doc.select("p.box-cfu-p description").get(1);
-		uInfo.setStudentClass(studentClass.text() + " "
-				+ studentCurrClass.text() + " ANNO");
-
-		// Getting additional information
-		Elements addInform = doc.select("table[summary$=studente] td");
-		// scorre gli elementi td memorizzando il precedente, se il precedente
-		// e' l'indicazione di una
-		// informazione utile allora vuol dire che quello attuale e'
-		// l'informazione da recuperare
-		String precedente = "";
-		for (Element tableCell : addInform) {
-			if (precedente.equals("Tasse")) { // situazione tasse es: regolare
-				uInfo.setTaxes(tableCell.text());
-			} else if (precedente.equals("Piano carriera")) { // se il piano
-																// carriera e'
-																// modificabile
-				uInfo.setCareerPlan(tableCell.text());
-			} else if (precedente.equals("Appelli disponibili")) { // quanti
-																	// appelli
-																	// disponibili
-																	// ci sono
-				uInfo.setAvailableExams(tableCell.text());
-			} else if (precedente.equals("Iscrizioni appelli")) { // a quanti
-																	// appelli
-																	// lo
-																	// studente
-																	// e'
-																	// iscritto
-				uInfo.setEnrolledExams(tableCell.text());
-			}
-			precedente = tableCell.text();
-		}
-
-		return uInfo;
-	}
-
-	/**
-	 * Metodo privato che controlla se il login ha avuto successo
-	 *
-	 * @param doc
-	 *            il Document da parsare
-	 * @return TRUE se il login ha avuto successo FALSE altrimenti
-	 */
-	private boolean isLogged(Document doc) {
-		Element firstMeta = doc.select("meta").first();
-
-		// quando il login non ha successo si ottiene una pagina con tag meta
-		// per il redirect
-		if (firstMeta.attr("http-equiv").equals("refresh")) {
-			return false; // login non riuscito
-		}
-
-		return true;
-	}
-
-	/**
-	 * Recupera la matricola da un'altra pagina! dato che non &egrave; presente
-	 * nella home di esse3
-	 *
-	 * @return lo studentID
-	 */
-	private String getStudentID(String username, String password)
-			throws MalformedURLException, UnirestException {
-
-		HTMLRequester req = new HTMLRequester();
-		String resPage = req.get(new URL(ConfigurationManager.getInstance().getRecordBookUrl()),
-				username, password);
-
-		Document doc = Jsoup.parse(resPage);
-		Element name = doc.select("div.titolopagina").first();
-
-		/**
-		 * dato che la stringa ha questa forma:
-		 * "Libretto di : IVAN DI RIENZO - [MAT. 148432]" elimina la parentesi
-		 * chiusa e infine splitta dividendo da "MAT. " e prende la seconda
-		 * stringa ottenuta cioe' la matricola
-		 */
-		String ID = name.text().replaceAll("]", "").split("MAT. ")[1];
-
-		return ID;
-	}
-
+        /**
+         * dato che la stringa ha questa forma: "Libretto di : IVAN DI RIENZO -
+         * [MAT. 148432]" elimina la parentesi chiusa e infine splitta dividendo
+         * da "MAT. " e prende la seconda stringa ottenuta cioe' la matricola
+         */
+        String ID = name.text().replaceAll("]", "").split("MAT. ")[1];
+        
+        return ID;
+    }
+    
 }
--- src/java/it/unimol/my/login/UserInformation.java
+++ src/java/it/unimol/my/login/UserInformation.java
@@ -1,17 +1,18 @@
 package it.unimol.my.login;
 
 /**
- * Classe bean che rappresenta le informazioni dell'utente recuperate dopo il login
- * 
+ * Classe bean che rappresenta le informazioni dell'utente recuperate dopo il
+ * login
+ *
  * @author Ivan Di Rienzo
  */
 public class UserInformation {
-    
+
     /* result sarà sempre success dato che se l'oggetto viene creato vuol dire che il login ha avuto successo! 
      * serve a facilitare la generazione del JSON tramite Gson (pigrizia XD)
      */
-    private String result = "success"; 
-    
+    private String result = "success";
+
     private String name; //nome
     private String surname; //cognome
     private String studentID; //matricola
@@ -20,17 +21,19 @@ public class UserInformation {
     private String careerPlan; //se il piano carriera è modificabile
     private String availableExams; // # appelli disponibili
     private String enrolledExams; // # esami prenotati
+    private String course;
+    private String department;
+    private String coursePath;
+    private String courseLength;
+    private String registrationDate;
 
-    
     /**
      * Bean class, costruttore vuoto
      */
     public UserInformation() {
     }
 
-    
     //SOLO GETTER E SETTER
-
     public String getName() {
         return name;
     }
@@ -46,7 +49,6 @@ public class UserInformation {
     public void setSurname(String surname) {
         this.surname = surname;
     }
-    
 
     public String getStudentID() {
         return studentID;
@@ -95,8 +97,45 @@ public class UserInformation {
     public void setEnrolledExams(String enrolledExams) {
         this.enrolledExams = enrolledExams;
     }
-    
-    
-    
-    
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getCoursePath() {
+        return coursePath;
+    }
+
+    public void setCoursePath(String coursePath) {
+        this.coursePath = coursePath;
+    }
+
+    public String getCourseLength() {
+        return courseLength;
+    }
+
+    public void setCourseLength(String courseLength) {
+        this.courseLength = courseLength;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
 }
