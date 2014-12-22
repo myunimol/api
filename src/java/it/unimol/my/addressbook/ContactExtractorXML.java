cb5ef211ee0fa8a7b6fa617139fe1f868f722501
Matteo Merola <mattmezza@gmail.com>
2014-12-22 21:57:37 +0100

    Modified API getAddressBook: just one search field.

--- src/java/it/unimol/my/addressbook/ContactExtractorXML.java
+++ src/java/it/unimol/my/addressbook/ContactExtractorXML.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.StringTokenizer;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -17,42 +17,86 @@ import org.jsoup.select.Elements;
  */
 public class ContactExtractorXML {
 
-    /**
-     * Il metodo getContact estrae un contatto da xml
-     *
-     * @param nome Il nome desiderato
-     * @param cognome Il cognome desiderato
-     * @return Una lista di contatti che soddisfa i parametri di input
-     */
-    public List<Contact> getContact(String name, String surname) {
+	/**
+	 * Il metodo getContact estrae un contatto da xml
+	 *
+	 * @param nome
+	 *            Il nome desiderato
+	 * @param cognome
+	 *            Il cognome desiderato
+	 * @return Una lista di contatti che soddisfa i parametri di input
+	 */
+	public List<Contact> searchContacts(String name, String surname) {
 
-        List<Contact> contactList = new ArrayList<Contact>();
+		List<Contact> contactList = new ArrayList<Contact>();
 
-        try {
-            File file = new File(ContactExtractorXML.class.getResource("addressBook.xml").getPath());            
-            Document doc = Jsoup.parse(file, "UTF-8");
-            Elements contatti = doc.select("contact");
-            
-            name = name.toLowerCase();
+		try {
+			File file = new File(ContactExtractorXML.class.getResource(
+					"addressBook.xml").getPath());
+			Document doc = Jsoup.parse(file, "UTF-8");
+			Elements contatti = doc.select("contact");
+
+			name = name.toLowerCase();
 			surname = surname.toLowerCase();
-            for (Element voce : contatti) {
-            	String fullname = voce.attr("fullname").toLowerCase();
-                if (fullname.contains(name)
-                        && fullname.contains(surname)) {
-                    Contact contact = new Contact();
-                    contact.setFullName(voce.attr("fullname"));
-                    contact.setRole(voce.attr("role"));
-                    contact.setBuilding(voce.attr("building"));
-                    contact.setInternalTelephone(voce.attr("internalTelephone"));
-                    contact.setExternalTelephone(voce.attr("externalTelephone"));                    
-                    contact.setEmail(voce.attr("email"));
-                    contactList.add(contact);
-                }
-            }
+			for (Element voce : contatti) {
+				String fullname = voce.attr("fullname").toLowerCase();
+				if (fullname.contains(name) && fullname.contains(surname)) {
+					Contact contact = new Contact();
+					contact.setFullName(voce.attr("fullname"));
+					contact.setRole(voce.attr("role"));
+					contact.setBuilding(voce.attr("building"));
+					contact.setInternalTelephone(voce.attr("internalTelephone"));
+					contact.setExternalTelephone(voce.attr("externalTelephone"));
+					contact.setEmail(voce.attr("email"));
+					contactList.add(contact);
+				}
+			}
 
-        } catch (IOException ex) {
-            Logger.getLogger(AddressBookExtractor.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return contactList;
-    }
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return contactList;
+	}
+
+	public List<Contact> searchContacts(String query) {
+		List<Contact> contactList = new ArrayList<Contact>();
+
+		try {
+			File file = new File(ContactExtractorXML.class.getResource(
+					"addressBook.xml").getPath());
+			Document doc = Jsoup.parse(file, "UTF-8");
+			Elements contatti = doc.select("contact");
+
+			StringTokenizer stringTokenizer = new StringTokenizer(query.trim(),
+					" ");
+			List<String> tokens = new ArrayList<String>();
+			while (stringTokenizer.hasMoreTokens()) {
+				String search = stringTokenizer.nextToken().toLowerCase();
+				tokens.add(search);
+			}
+			for (Element voce : contatti) {
+				String fullname = voce.attr("fullname").toLowerCase();
+				for (String token : tokens) {
+					if (fullname.contains(token)) {
+						Contact contact = new Contact();
+						contact.setFullName(voce.attr("fullname"));
+						contact.setRole(voce.attr("role"));
+						contact.setBuilding(voce.attr("building"));
+						contact.setInternalTelephone(voce
+								.attr("internalTelephone"));
+						contact.setExternalTelephone(voce
+								.attr("externalTelephone"));
+						contact.setEmail(voce.attr("email"));
+						if (!contactList.contains(contact)) {
+							contactList.add(contact);
+						}
+					}
+				}
+			}
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return contactList;
+	}
 }
---
+++ src/java/it/unimol/my/addressbook/SearchContactsServlet.java
@@ -0,0 +1,54 @@
+package it.unimol.my.addressbook;
+
+import it.unimol.my.utils.WebServiceServlet;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Carlo Branca
+ */
+@WebServlet(name = "ContactServlet", urlPatterns = { "/searchContacts" })
+public class SearchContactsServlet extends WebServiceServlet {
+
+	/**
+	 * Lo uid seriale della versione.
+	 */
+	private static final long serialVersionUID = -4695089031840096808L;
+
+	@Override
+	protected void serve(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		// recupero l'estrattore
+		ContactExtractorXML contactExtractor = new ContactExtractorXML();
+		// estraggo il libretto degli esami
+		try {
+			String search = req.getParameter("search");
+			if (search == null) {
+				String msg = config.getMessage("badParameters");
+				writer.println("{\"result\":\"failure\",\"msg\":\"" + msg
+						+ "\"}");
+				return;
+			}
+			List<Contact> contactList = contactExtractor.searchContacts(search);
+			if (contactList == null) {
+				String unknownErrorMsg = config.getMessage("unknownError");
+				writer.println("{\"result\":\"failure\",\"msg\":\""
+						+ unknownErrorMsg + "\"}");
+				return;
+			}
+			// converto il libretto in json
+			String json = gson.toJson(contactList);
+			// stampo il json a video
+			writer.println("{\"result\":\"success\",\"contacts\":" + json + "}");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			writer.close();
+		}
+	}
+
+}
