bb99b7175ce74b97249cc3686b59203ee371ecf9
Matteo Merola <mattmezza@gmail.com>
2015-01-14 18:50:00 +0100

    Fixed a bug into NewsExtractor.java when no news text is available

--- src/java/it/unimol/my/news/NewsExtractor.java
+++ src/java/it/unimol/my/news/NewsExtractor.java
@@ -27,15 +27,45 @@ public class NewsExtractor implements NewsExtractorInterface {
 			Elements html = doc.select("div[class=posts_wrapper clearfix]");
 			Elements news = html.select("div[id^=post-]");
 
-			for (Element element : news) {
-				String title = element.select("div[class=desc]").first()
-						.child(0).text();
-				String text = element.select("div[class=desc]").first()
-						.child(1).text();
-				String link = element.select("div[class=desc]").first()
-						.select("a").attr("href");
-				String date = element.select("div[class=date]").text();
-				News singleNews = new News(date, title, text, link);
+			for (Element postDiv : news) {
+				if (postDiv.children().size() <= 0) {
+					continue;
+				}
+				Element descDiv = postDiv.select("div[class=desc]").first();
+
+				String titleString = "Notizia senza titolo";
+				String linkString = url;
+				if (descDiv.children().size() > 0) {
+					Element title = descDiv.child(0);
+					if (!title.text().equals("")) {
+						titleString = title.text();
+					}
+					Element a = descDiv.select("a").first();
+					if (a != null) {
+						linkString = a.attr("href");
+					}
+				}
+
+				String textString = "Notizia senza testo";
+				if (descDiv.children().size() > 1) {
+					Element text = descDiv.child(1);
+					if (!text.text().equals("")) {
+						textString = text.text();
+					}
+				}
+
+				String dateString = "Data non disponibile";
+				try {
+					Element date = postDiv.select("div[class=date]").first();
+					if (date != null) {
+						dateString = date.text();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+
+				}
+				News singleNews = new News(dateString, titleString, textString,
+						linkString);
 				newsList.add(singleNews);
 			}
 
